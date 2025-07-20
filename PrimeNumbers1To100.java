e5b04b586caeb3b521cc169698f9f67177a12661
Cyr1ac <cyriacjosekallolil2028@cs.sjcetpalai.ac.in>
2025-07-20 08:29:46 +0530

    Add files via upload

---
+++ PrimeNumbers1To100.java
@@ -0,0 +1,27 @@
+
+    public class PrimeNumbers1To100 {
+    public static void main(String[] args) {
+        System.out.println("Prime numbers between 1 and 100:");
+
+        
+        for (int number = 2; number <= 100; number++) {
+            boolean isPrime = true;
+
+
+            for (int i = 2; i < number; i++) {
+                if (number % i == 0) {
+                    isPrime = false;
+                    break; 
+                }
+            }
+
+            if (isPrime) {
+                System.out.print(number + " ");
+            }
+        }
+    }
+}
+
+
+    
+
